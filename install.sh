fc55c4e0159886d35c2f7a79180bac2bf416c2b2
PaulaViriato <paulaviriato@dcc.ufmg.br>
2021-08-16 10:16:31 -0300

    Requirements
    Requirements

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1uZOhZl3oKXf66DCCZIE2O7Aax3OJzfvl'
+wget https://www.dropbox.com/s/7ski7orlpo67y3s/all_0729_sd11_lr0.0001_bs2_ga16.zip
 unzip all_0729_sd11_lr0.0001_bs2_ga16.zip
 mkdir experiments
 mkdir experiments/all_0729_sd11_lr0.0001_bs2_ga16
